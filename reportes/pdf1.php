0f0e56ec76b2e54c25b2c7bd5a0ede41091895d7
notario <rafaelnotariorodriguez@gmail.com>
2018-09-13 12:23:15 -0500

    USE GIT CHECKOUT -B NEW PARA DETACHMENT DEL BRANCH PRINCIPAL NO SE PODIA HACER EL PUSH REMOTE

---
+++ reportes/pdf1.php
@@ -0,0 +1,12 @@
+<?php 
+
+require '../vendor/autoload.php';
+
+use Spipu\Html2Pdf\Html2Pdf;
+
+$html2pdf = new Html2Pdf();
+$html2pdf->writeHTML('<h1> CONTRATO FF</h1>');
+$html2pdf->output();
+
+
+ ?>
